930c3b9278bdefdd980dd3db8513e988ff42736b
ap8690 <ajaypalsingh1082001@gmail.com>
2024-08-27 06:07:26 +0530

    all contracts

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -66,7 +66,7 @@ export const WETH = {
   ),
   [ChainId.POLYGON]: new Token(
     ChainId.POLYGON,
-    '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+    '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
     18,
     'WMATIC',
     'Wrapped Native'
